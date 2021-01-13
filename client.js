d026b70ab7a7339d558eb451361687242e735ff1
Ali Bas <alibas01@gmail.com>
2021-01-13 13:31:00 -0500

    now we have mesaages and name on server

--- client.js
+++ client.js
@@ -7,16 +7,30 @@ const connect = function() {
   const conn = net.createConnection({ 
     host: "localhost",
     port: 50541
+  }, () => {
+    console.log("Successfully connected to game server"  );
+    conn.write("Name: ALB");
   });
-  // interpret incoming data as text
-  // const stdin = process.stdin;
-  // stdin.setRawMode(true);
-  // stdin.setEncoding('utf8');
+  
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
   conn.setEncoding('utf8'); 
 
+  //send the name to server
+  // const initials = 'ALB';
+  // stdin.on('data', (input) => {
+  //   client.write(`"Name: ${initials}" `);
+  // })
+
+  //if receive a data from server, show it!
   conn.on('data', (data) => {
     console.log(data);
   })
+  // if connection ends, show it
+  conn.on('end', () => {
+    console.log('disconnected from server');
+  });
 
   return conn;
 }
